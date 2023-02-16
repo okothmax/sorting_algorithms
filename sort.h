3e23e9633353ba49c7b8916450113374642669d2
okothmax <okothmaxwell03@gmail.com>
2023-02-16 17:57:14 +0300

    added all prototypes

--- sort.h
+++ sort.h
@@ -1,8 +1,8 @@
-#ifndef _SORT_H_
-#define _SORT_H_
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef SORT_H
+#define SORT_H
 
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -17,12 +17,20 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
+listint_t *swap_node(listint_t *node, listint_t **list);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
-#endif/* _SORT_H_ */
+#endif
